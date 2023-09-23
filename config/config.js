5c97367d85a9bf2994f5868972a71bcf2d7ba994
homelesscat <gilbertperezdt@gmail.com>
2023-09-23 12:34:39 -0700

    connected to DB,

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-      process.env.DB_NAME,
+      process.env.DB,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
